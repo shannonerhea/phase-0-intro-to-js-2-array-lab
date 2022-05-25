e08ffb4ae0f8660e8721235c1cda23dae394e222
shannone <shannone426@gmail.com>
2022-05-25 13:04:02 -0400

    commit tree 7e61a29b65fdc2b992d9ece6aa6b80928a728a68

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+function appendCat(name) {
+    const newCatsArr = [...cats, name];
+    return newCatsArr;
+};
+function prependCat(name) {
+    return prependCat = [name, ...cats];
+   
+};
+function removeLastCat() {
+    return cats.slice(0, -1);
+};
+function removeFirstCat() {
+    return cats.slice(1);
+};
